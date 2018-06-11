dbf598357899b38e6a62294974ae5de82cfe5b24
wendellotim <wendellzknown2u@gmail.com>
2018-06-11 13:31:22 +0300

    Function to subtract numbers

--- Add.js
+++ Add.js
@@ -1,7 +1,10 @@
 
-function Add (number1, number2);{
- number1 = 5;
- number2 = 7;
- return(number1 + number2)
- console.log(Add);
+function Add(number1, number2){
+    return (number1 + number2);
+
 }
+console.log(Add(5,7));
+function Subtraction(number1, number2){
+    return(number1 - number2);
+}
+console.log(subtract(8,4));
